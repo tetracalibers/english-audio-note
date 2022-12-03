cea2d78ccb31275334ed179d85a3f9b15131007d
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-12-03 17:19:16 +0900

    繰り返し読み上げるフレーズコンポーネントをつくった

---
+++ src/components/audio/phrase.svelte
@@ -0,0 +1,96 @@
+<script lang="ts">
+  import { onMount } from "svelte"
+  import PlayIcon from "../icons/play-icon.svelte"
+  import StopIcon from "../icons/stop-icon.svelte"
+
+  export let text: string
+
+  let speaker: SpeechSynthesisUtterance
+
+  const speak = () => {
+    window.speechSynthesis.speak(speaker)
+    speaker.onend = speak
+  }
+
+  const stop = () => {
+    speaker.onend = null
+    window.speechSynthesis.cancel()
+  }
+
+  const loadVoice = () => {
+    const voices = speechSynthesis.getVoices()
+    speaker.voice = voices.find(function (voice) {
+      return voice.name === "Google US English"
+    })
+  }
+
+  onMount(() => {
+    // 発言を設定
+    speaker = new SpeechSynthesisUtterance()
+    // テキストを設定
+    speaker.text = text
+    // 言語を設定
+    speaker.lang = "en-US"
+    // 速度を設定
+    speaker.rate = 0.5
+    // 英語に対応しているvoiceを設定
+    loadVoice()
+    window.speechSynthesis.onvoiceschanged = loadVoice
+  })
+</script>
+
+<div class="PhraseSpeaker">
+  <div class="PhraseSpeaker__buttons">
+    <button
+      class="PhraseSpeaker-Control --speak"
+      on:click={speak}
+      aria-label="speak"
+    >
+      <PlayIcon size="1rem" />
+    </button>
+    <button
+      class="PhraseSpeaker-Control --stop"
+      on:click={stop}
+      aria-label="stop"
+    >
+      <StopIcon size="1rem" />
+    </button>
+  </div>
+  <p class="PhraseSpeaker__phrase">{text}</p>
+</div>
+
+<style>
+  .PhraseSpeaker {
+    display: flex;
+  }
+
+  .PhraseSpeaker__phrase {
+    font-size: 1.5rem;
+  }
+
+  .PhraseSpeaker__buttons {
+    display: flex;
+    gap: 0.5rem;
+    padding-right: 2rem;
+    height: 2.5rem;
+  }
+
+  .PhraseSpeaker-Control {
+    border-radius: 0.75rem;
+    width: 2rem;
+    height: 2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+    margin: auto 0;
+  }
+
+  .PhraseSpeaker-Control.--speak {
+    background-image: linear-gradient(to right, #3a7bd5 0%, #00d2ff 100%);
+  }
+
+  .PhraseSpeaker-Control.--stop {
+    background-image: linear-gradient(to right, #ff512f 0%, #dd2476 100%);
+  }
+</style>
