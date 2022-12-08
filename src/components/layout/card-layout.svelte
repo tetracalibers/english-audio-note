9d5d51c046f026464063e5d17cb0dbf9b74db577
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-12-08 18:34:30 +0900

    CardLayoutをコンポーネント化

---
+++ src/components/layout/card-layout.svelte
@@ -0,0 +1,10 @@
+<div class="CardLayout"><slot /></div>
+
+<style>
+  .CardLayout {
+    display: grid;
+    gap: 20px;
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+    padding: 2.5rem 0;
+  }
+</style>
